1c1e3dc8f43331b59073ff474330edadd74ad1dc
Mark <18929562811@163.com>
2016-10-24 21:56:38 +0800

    关于swift的方法命名的规范

---
+++ Swift(方法命名方式)/Swift(方法命名方式)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift(方法命名方式)
+//
+//  Created by 叶杨杨 on 2016/10/24.
+//  Copyright © 2016年 叶杨杨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
