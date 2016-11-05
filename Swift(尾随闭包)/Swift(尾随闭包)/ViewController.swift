5aa6b9577852075762988b49e891a2f89b2f8028
Mark <18929562811@163.com>
2016-11-05 14:44:24 +0800

    尾随闭包的写法，以及GCD中嵌套闭包的写法

--- Swift(尾随闭包)/Swift(尾随闭包)/ViewController.swift
+++ Swift(尾随闭包)/Swift(尾随闭包)/ViewController.swift
@@ -14,13 +14,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //闭包回调传递数据
+        //闭包回调传递数据,尾随闭包:如果函数的最后一个参数是闭包，那函数参数可以提前结束，最后一个参数直接使用{}来代替 包装闭包的代码
+        
+        //尾随闭包写法
         loadData { (result) in
             
             print("获取的新闻数据\(result)")
         }
+        
+        //原始的写法
+        loadData(completion: { (result) -> ()  in
+        
+            print(result)
+        
+        })
+        
+        
+        DispatchQueue.global().async { 
+            
+            //嵌套GCD异步时内部的嵌套的xcode不会自动转换为尾随闭包
+            DispatchQueue.main.async(execute: { 
+                
+            })
+            
+            DispatchQueue.main.async{
+                
+            }
+        }
+        
     }
 
+    //一般的异步函数呢都会使用逃逸闭包，就是逃脱这个函数，等到有结果是才调用这个闭包
     func  loadData(completion:@escaping (_ result:[String]) -> ()) -> () {
     
         //GCD基本语法
@@ -39,7 +63,6 @@ class ViewController: UIViewController {
                 print("主线程更新UI\(Thread.current)")
                 //回调执行 
                 completion(json)
-
             }
         }
     }
