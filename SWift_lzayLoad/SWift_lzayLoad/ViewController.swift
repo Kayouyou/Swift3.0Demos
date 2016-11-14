d5575c75a56ca1601f72e91cf668a6c55586a3c5
Mark <18929562811@163.com>
2016-11-14 22:02:16 +0800

    懒加载完整写法

--- SWift_lzayLoad/SWift_lzayLoad/ViewController.swift
+++ SWift_lzayLoad/SWift_lzayLoad/ViewController.swift
@@ -15,12 +15,33 @@ class ViewController: UIViewController {
     //正常的声明属性
 //    var label: DemoLable?
     
-    //懒加载
+    //1.懒加载基本写法
+    
     /**
      1,能够延迟加载
      2，最大的好处就是可以解除解包的烦恼，后面不用？ ！不用担心他没有值
     */
-    lazy var label:DemoLable = DemoLable()
+    
+//    lazy var label:DemoLable = DemoLable()
+
+    
+    
+    //2,懒加载其实是闭包
+    //完整写法
+    /**
+    {} 包装代码
+    () 执行代码
+     日常开发不建议这样写，闭包中只能提示不好，self.还注意循环引用
+    */
+    lazy var label = { ()->DemoLable in
+        let l = DemoLable()
+        
+        //设置lable属性
+        
+        return l
+    }()
+    
+    
     
     
     
