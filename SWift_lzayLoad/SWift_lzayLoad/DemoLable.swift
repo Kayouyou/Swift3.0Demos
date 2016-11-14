b46fc1df48f320a2a579cf8411f28a4e25820626
Mark <18929562811@163.com>
2016-11-14 21:53:31 +0800

    懒加载实现

---
+++ SWift_lzayLoad/SWift_lzayLoad/DemoLable.swift
@@ -0,0 +1,21 @@
+//
+//  DemoLable.swift
+//  SWift_lzayLoad
+//
+//  Created by 叶杨杨 on 2016/11/14.
+//  Copyright © 2016年 叶杨杨. All rights reserved.
+//
+
+import UIKit
+
+class DemoLable: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- SWift_lzayLoad/SWift_lzayLoad/ViewController.swift
+++ SWift_lzayLoad/SWift_lzayLoad/ViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
+
+/// swift 懒加载
 class ViewController: UIViewController {
 
+    //正常的声明属性
+//    var label: DemoLable?
+    
+    //懒加载
+    /**
+     1,能够延迟加载
+     2，最大的好处就是可以解除解包的烦恼，后面不用？ ！不用担心他没有值
+    */
+    lazy var label:DemoLable = DemoLable()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
