2439417d3980cba17c440cf1c583aab08e79dc7f
Mark <18929562811@163.com>
2016-11-19 14:15:53 +0800

    基本架构搭建

---
+++ Swift_微博/Swift_微博/Classes/View/Main/WBMainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WBMainViewController.swift
+//  Swift_微博
+//
+//  Created by 叶杨杨 on 2016/11/19.
+//  Copyright © 2016年 叶杨杨. All rights reserved.
+//
+
+import UIKit
+
+
+/// 主控制器
+class WBMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
