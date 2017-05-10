b3f3f5e039a75b7b16d9da60dbe92c99d597626c
Mark <18929562811@163.com>
2017-05-10 19:51:11 +0800

    swift 的关联类型，以及注意点

--- Swift_Tips/NameSpace.playground/Contents.swift
+++ Swift_Tips/NameSpace.playground/Contents.swift
@@ -28,3 +28,70 @@ protocol Dog{}
 //给它一个更加易读的名字，增强代码可读性！
 typealias Pat  = Cat & Dog
 
+
+//4,associatedtype，关联类型
+//swift中协议中可以定义方法和属性
+protocol Food{}
+
+protocol Animal{
+
+    //实际上协议中我们还可以定义类型的占位符，让实现协议的类型来指定具体的类型；
+    //使用associatedtype可以加一个限定，让具体实现协议的动物指定具体的食物类型；
+    associatedtype F
+    func eat(_food: F) //后面的{} 加不加都OK的
+}
+
+struct Meat: Food{}
+struct Grass: Food{}
+
+//由于老虎是不吃素食的，怎么在编译的时候就确保这一条件呢
+struct Tiger:Animal{
+    
+    //在Tiger实现animal了协议之前，animal根本不关系F是什么类型，只需要F 和 eat参数中的类型一致即可；
+    //tiger中只要实现了正确的eat，就会自耦东推断出F是meat，所以无需指定 F = Meat
+    func eat(_food: Meat) {
+        
+    }
+}
+
+
+// 需要注意的是： 添加了关联类型后，Animal协议就不能当做独立的类型使用了，试想我们有一个函数来判断动物的类型是否危险？
+//❌ 写法
+
+ 
+//func isDangerous(animal:Animal) -> Bool{
+//    
+//    if animal is Tiger {
+//        return true
+//    }else{
+//        return false
+//    }
+//}
+
+
+// 如果直接向上面这样写，xcode会报错，说当一个协议加入了泛型associatedtype或者 self的约束后，它只能用来泛型约束，而不能作为独立的类型的占位使用了；也失去了动态派发的特性；
+
+// 导致以上报错的原因是： swift需要在编译的时候就确定所有的类型，这是因为animal包含了一个不确定的类型，所以随着animal本身类型的的变化，其中的F 将无法确定（eat参数你无法确定的），
+
+//解决办法是，我们只能将以上的函数改为泛型约束！
+
+func IsDangerous< T:Animal>(animal:T) -> Bool{
+    
+    if animal is Tiger {
+        return true
+    }else{
+        return false
+    }
+}
+
+IsDangerous(animal: Tiger())
+
+
+
+
+
+
+
+
+
+
